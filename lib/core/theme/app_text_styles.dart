8d871ebc8e147c36c12fd33a11eb86506f805c04
abdelrhman-mostafa95 <abdelrhman.mostafa1095@gmail.com>
2025-12-10 17:01:59 +0200

    create project and main folders

---
+++ lib/core/theme/app_text_styles.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF5117AC);
+  static const Color gradientFirstColor = Color(0xFF4A1192);
+  static const Color gradientSecondColor = Color(0xFF2CD5C4);
+}
